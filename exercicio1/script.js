ef55873007fa11b3187765aeab1896c59b4a951d
Anacxx <anacarolina.clcx@gmail.com>
2023-03-22 14:40:01 -0300

    Minha resolução

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+var arrayStrings = ["Ana","Maristela","Emily","Poly","Bela","Cristalina","Louie","Zara"]
+var numeros = [32,452,65,76,34,654,63,4556,345,345,52,2]
+var mistura = ["Ana",29,true]
+console.log(arrayStrings)
+console.log(numeros)
+console.log(mistura)
---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+var numeros = [32,452,65,76,34,654,63,4556,345,345,52,2]
+var arrayStrings = ["Ana","Maristela","Emily","Poly","Bela","Cristalina","Louie","Zara"]
+var mistura = ["Ana",29,"Emily",true]
+console.log(arrayStrings.length)
+console.log(numeros.length)
+console.log(mistura.length)
+console.log(numeros[0])
+console.log(arrayStrings[1])
+console.log(mistura[2])
+console.log(`Possui o número 9 na lista? ${numeros.includes(9)}`)
+console.log(`Emily está na lista? ${arrayStrings.includes("Emily")}`)
